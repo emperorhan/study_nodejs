2552ccbd459d9ba5c8cdcbb819b10d783a6b4fa7
Emperorhan <Emperorhan@Emperorhanui-MacBook.local>
2017-12-19 18:03:21 +0900

    12/19 18:02 study - 모듈 3 까지

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
-const http = require('http');
+const http = require('http');	//	app이 구동되기 위해서는 http라는 모듈이 요구된다 리턴 값을 http에 담음, const로 상수(한번 로드해오면 바뀌지 않는다)
  
-const hostname = '127.0.0.1';
-const port = 1337;
+const hostname = '127.0.0.1';	//	
+const port = 1338;
  
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
